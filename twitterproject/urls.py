77327ad1a15cf704b7d4a3a057ce591443b04ffd
unknown <anne_linde@yahoo.dk>
2019-10-13 14:29:08 +0200

    styling added

--- twitterproject/urls.py
+++ twitterproject/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('loginapp.urls', namespace='loginapp')),
     path('twitter/', include('twitterapp.urls', namespace='twitterapp')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
